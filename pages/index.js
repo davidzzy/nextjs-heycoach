6046127994e92c88f6186541355844d7798416b9
davidzzy <davidzhuzhenyi@gmail.com>
2021-01-25 16:31:40 -0500

    test with map selection

--- pages/index.js
+++ pages/index.js
@@ -21,20 +21,9 @@ export default function Home({ allPostsData }) {
 
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
+        <Link href={`/posts/playerSelection`}>
+      <a>Player Select</a>
+      </Link>
       </section>
     </Layout>
   )
